8ad2bd8614eda049a45793c3c8892e73b3d815b1
Jacob LeCoq <lecoqjacob@gmail.com>
2026-08-23 13:59:08 -0500

    fix(cli): default daemon logging to info

--- crates/bg-cli/src/bin/bgd.rs
+++ crates/bg-cli/src/bin/bgd.rs
@@ -5,6 +5,8 @@ use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()))
+        .init();
     bg_daemon::run().await
 }
